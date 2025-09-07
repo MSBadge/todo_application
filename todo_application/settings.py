22b8d6cc8bd8d9affbf86e5142d8012518ecafb0
Mukul Badge <badgemukul36@gmail.com>
2025-09-07 17:25:12 +0530

    1.1

--- todo_application/settings.py
+++ todo_application/settings.py
@@ -123,8 +123,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'static/'
+
+
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
